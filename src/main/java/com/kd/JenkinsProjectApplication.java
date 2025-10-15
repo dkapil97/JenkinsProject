27cdb44d54ff220e43c6906745a4c445e2c291c6
dkapil97 <kapildhakade1902@gmail.com>
2025-10-15 23:46:04 +0530

    second commit Jenkins Project

--- src/main/java/com/kd/JenkinsProjectApplication.java
+++ src/main/java/com/kd/JenkinsProjectApplication.java
@@ -1,12 +1,24 @@
 package com.kd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class JenkinsProjectApplication {
-
+	
+	public static Logger logger=LoggerFactory.getLogger(JenkinsProjectApplication.class);
+	
+	@PostConstruct
+	public  void initi() {
+		logger.info("Application is Started..!");
+	}
+	
 	public static void main(String[] args) {
+		logger.info("Application is executed...!");
 		SpringApplication.run(JenkinsProjectApplication.class, args);
 	}
 
--- src/test/java/com/kd/JenkinsProjectApplicationTests.java
+++ src/test/java/com/kd/JenkinsProjectApplicationTests.java
@@ -1,13 +1,23 @@
 package com.kd;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsProjectApplicationTests {
 
+	public static Logger logger=LoggerFactory.getLogger(JenkinsProjectApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("test case executed..!");
+		assertWith(true, true);
+	}
+
+	private void assertWith(boolean b, boolean c) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
